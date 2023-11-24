9a8fbac564e92ec7c0063194967d37a61a4a3df2
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-11-24 10:04:29 +0530

    checked support and added prompt

--- ReviseApp1/app/src/main/java/com/example/reviseapp1/MainActivity.kt
+++ ReviseApp1/app/src/main/java/com/example/reviseapp1/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.reviseapp1
 
 import android.content.Intent
+import android.hardware.biometrics.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import android.hardware.biometrics.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.viewModels
+import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.viewModels
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators
+import androidx.biometric.BiometricPrompt
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import com.example.reviseapp1.database.BaseApplication
 import com.example.reviseapp1.databinding.ActivityMainBinding
-import com.example.reviseapp1.viewModel.FragmentViewModel
-import com.example.reviseapp1.viewModel.FragmentViewModelFactory
 
 private const val TAG = "MainActivity tag"
 
@@ -19,12 +23,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHost = supportFragmentManager.findFragmentById(binding.navHostFragmentContainer.id) as NavHostFragment
+        checkBiometricSupport()
+        val navHost =
+            supportFragmentManager.findFragmentById(binding.navHostFragmentContainer.id) as NavHostFragment
         navController = navHost.navController
 
         binding.nextbutton.setOnClickListener {
@@ -33,6 +40,46 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("STRING_VALUE", textValue)
             startActivity(intent)
         }
+
+        binding.checkBiometricsbutton.setOnClickListener {
+            //create prompt
+            val promptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setSubtitle("Log in using your biometric credential")
+                .setAllowedAuthenticators(Authenticators.BIOMETRIC_STRONG or Authenticators.DEVICE_CREDENTIAL)
+                .build()
+        }
+    }
+
+    private fun checkBiometricSupport() {
+        val biometricManager = BiometricManager.from(this)
+
+        when (biometricManager.canAuthenticate(Authenticators.BIOMETRIC_STRONG or Authenticators.DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                notifyUser("You can use fingerprint for login")
+                binding.checkBiometricsbutton.visibility = View.VISIBLE
+            }
+
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                notifyUser("No biometric features available on this device.")
+                binding.checkBiometricsbutton.visibility = View.INVISIBLE
+            }
+
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                notifyUser("Biometric features are currently unavailable.")
+                binding.checkBiometricsbutton.visibility = View.INVISIBLE
+            }
+
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                notifyUser("Your device doesn't have fingerprint saved,please check your security settings")
+                binding.checkBiometricsbutton.visibility = View.INVISIBLE
+            }
+
+        }
+    }
+
+    private fun notifyUser(s: String) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
     }
 
 }

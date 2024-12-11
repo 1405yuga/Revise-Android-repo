76fdf8d870a0e8267d9c7edba9df0a86f298d43d
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-12-11 21:32:10 +0530

    moved to fragments folder

--- PagerApp/app/src/main/java/com/example/pagerapp/fragments/CallsFragment.kt
+++ PagerApp/app/src/main/java/com/example/pagerapp/fragments/CallsFragment.kt
@@ -1,10 +1,11 @@
-package com.example.pagerapp
+package com.example.pagerapp.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.pagerapp.R
 
 class CallsFragment : Fragment() {
 

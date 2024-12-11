bf271d71e52332854b26e0f16bd964589e5d388c
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-12-11 21:47:28 +0530

    attached tabLayout

--- PagerApp/app/src/main/java/com/example/pagerapp/MainActivity.kt
+++ PagerApp/app/src/main/java/com/example/pagerapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.pagerapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pagerapp.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,6 +15,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.pager.adapter = ViewPagerAdapter(this)
+
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab: TabLayout.Tab, position: Int ->
+            when (position) {
+                0 -> {
+                    tab.text = "Chats"
+                    tab.setIcon(R.drawable.baseline_chat_24)
+                }
+
+                1 -> {
+                    tab.text = "Status"
+                    tab.setIcon(R.drawable.baseline_status_24)
+                }
+
+                2 -> {
+                    tab.text = "Calls"
+                    tab.setIcon(R.drawable.baseline_call_24)
+                }
+            }
+
+        }.attach()
     }
 
 }

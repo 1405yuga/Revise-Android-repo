47d08d82e0c9435d36e9378d032204bb708d6885
Vasaikar <yuga-ganesh.vasaikar@capgemini.com>
2024-12-12 15:37:16 +0530

    checked and attached biometrics

--- PagerApp/app/src/main/java/com/example/pagerapp/MainActivity.kt
+++ PagerApp/app/src/main/java/com/example/pagerapp/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.pagerapp
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.pagerapp.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -39,12 +48,69 @@ class MainActivity : AppCompatActivity() {
         }.attach()
 
         //Biometrics----------------------------------------------------------
-        val biometricManager = androidx.biometric.BiometricManager.from(this)
-        //check
-        when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL)) {
-            BiometricManager.BIOMETRIC_SUCCESS ->{
+        val biometricManager = BiometricManager.from(this)
+        //check for permission
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.USE_BIOMETRIC
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.USE_BIOMETRIC),
+                1
+            )
+        } else {
+            //check if biometrics available
+            when (biometricManager.canAuthenticate(
+                BiometricManager.Authenticators.BIOMETRIC_STRONG
+                        or BiometricManager.Authenticators.BIOMETRIC_WEAK
+                        or BiometricManager.Authenticators.DEVICE_CREDENTIAL
+            )) {
+                BiometricManager.BIOMETRIC_SUCCESS -> {
+                    showBiometricPrompt()
+                }
 
+                BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                    showToast("Biometric hardware not available")
+
+                BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                    showToast("Biometric credentials not enrolled")
+
+                else -> showToast("Some biometric error")
             }
         }
+
+    }
+
+    private fun showBiometricPrompt() {
+        val biometricPrompt = BiometricPrompt(this, ContextCompat.getMainExecutor(this),
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    showToast("Authentication error $errString")
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    showToast("Authenticated successfully!!")
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    showToast("Authentication failed")
+                }
+            })
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric Authentication")
+            .setSubtitle("Login with fingerprint")
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
 }

863b73b133f33750b4384954936cfdd4f86e714f
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-05-08 19:21:30 +0530

    fragments moved to pkg

--- Overall/app/src/main/java/com/example/overall/fragments/FirstFragment.kt
+++ Overall/app/src/main/java/com/example/overall/fragments/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.example.overall
+package com.example.overall.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.overall.R
 import com.example.overall.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
--- Overall/app/src/main/java/com/example/overall/fragments/SecondFragment.kt
+++ Overall/app/src/main/java/com/example/overall/fragments/SecondFragment.kt
@@ -1,4 +1,4 @@
-package com.example.overall
+package com.example.overall.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater

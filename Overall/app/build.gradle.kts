a867635b772dbbb8e5534f7670794bfe19db4208
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-05-09 10:48:10 +0530

    retrofit dependency in kotlin fmt

--- Overall/app/build.gradle.kts
+++ Overall/app/build.gradle.kts
@@ -52,6 +52,6 @@ dependencies {
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.ui)
     //retrofit----------------------------
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 }

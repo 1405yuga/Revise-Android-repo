93d8992682e6d15c0865210e2b4fc1b5df5acbfa
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-05-10 15:37:07 +0530

    created dataclass

---
+++ JavaTestApp/app/src/main/java/com/example/javatestapp/data/Item.java
@@ -0,0 +1,19 @@
+package com.example.javatestapp.data;
+
+public class Item {
+    private int id;
+    private String name;
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
---
+++ JavaTestApp/app/src/main/java/com/example/javatestapp/data/ItemResult.java
@@ -0,0 +1,12 @@
+package com.example.javatestapp.data;
+
+import java.util.List;
+
+public class ItemResult {
+    private List<Item> items;
+
+    public List<Item> getItems() {
+        return items;
+    }
+}
+

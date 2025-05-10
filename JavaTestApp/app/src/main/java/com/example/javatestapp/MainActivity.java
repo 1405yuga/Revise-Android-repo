a724d6c33ccaba35ab41afe690be8a728f25b5c7
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-05-10 15:36:51 +0530

    refactored

--- JavaTestApp/app/src/main/java/com/example/javatestapp/MainActivity.java
+++ JavaTestApp/app/src/main/java/com/example/javatestapp/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.javatestapp;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 

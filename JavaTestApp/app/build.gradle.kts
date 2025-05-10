7e672b0ceca6de7158e224d9aa4605e0c4c5cc21
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-05-10 15:35:44 +0530

    added retrofit dependency

--- JavaTestApp/app/build.gradle.kts
+++ JavaTestApp/app/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
     //navigation-------------------------
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    //retrofit----------------------
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }

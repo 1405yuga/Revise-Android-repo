034f72600b3c411b704108d0bdd0e97cbe44a5ff
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-07-07 13:22:14 +0530

    dynamic receiver implemented

--- BroadcastRecieverImpl/app/src/main/java/com/example/broadcastrecieverimpl/MainActivity.kt
+++ BroadcastRecieverImpl/app/src/main/java/com/example/broadcastrecieverimpl/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.broadcastrecieverimpl
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.broadcastrecieverimpl.dynamic_receiver.ChargingReceiver
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var chargingReceiver: ChargingReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        chargingReceiver = ChargingReceiver()
+        val filter = IntentFilter(Intent.ACTION_POWER_CONNECTED)
+        registerReceiver(chargingReceiver, filter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(chargingReceiver)
     }
 }

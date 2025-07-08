065668005d3a7587bb0dd915d3fac1352deac5d0
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-07-08 14:23:00 +0530

    observed the connection

--- NetworkCheck_Impl/app/src/main/java/com/example/networkcheck_impl/BaseApplication.kt
+++ NetworkCheck_Impl/app/src/main/java/com/example/networkcheck_impl/BaseApplication.kt
@@ -1,8 +1,17 @@
 package com.example.networkcheck_impl
 
 import android.app.Application
+import com.example.networkcheck_impl.network.NetworkConnectionObserver
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class BaseApplication: Application() {
+class BaseApplication : Application() {
+
+    @Inject
+    lateinit var networkConnectionObserver: NetworkConnectionObserver
+    override fun onCreate() {
+        super.onCreate()
+        networkConnectionObserver.register()
+    }
 }
--- NetworkCheck_Impl/app/src/main/java/com/example/networkcheck_impl/MainActivity.kt
+++ NetworkCheck_Impl/app/src/main/java/com/example/networkcheck_impl/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.networkcheck_impl
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.networkcheck_impl.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        viewModel.isConnected.observe(this) {
+            binding.textView.setText("Internet connected : $it")
         }
     }
 }
